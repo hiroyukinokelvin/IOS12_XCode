d40025c87feb29aa56530d72424d03ca52eecdf1
Mac <mac@macde-macbook-pro.local>
2019-03-17 22:06:44 +0800

    Initial Commit

---
+++ TestAppleApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestAppleApp
+//
+//  Created by Mac on 2019/3/17.
+//  Copyright © 2019 kelvin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
